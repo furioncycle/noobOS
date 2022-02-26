4052587ad04c7c1ca806e47a3a258851ecb7de2e
ttecho <admarquez21@gmail.com>
2022-02-25 23:25:55 -0800

    A minimal Rust Kernal - Finished

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 //Ensures the rusts compiler outputs the functions
 //name _start
 #[no_mangle]
@@ -14,6 +16,15 @@ pub extern "C" fn _start() -> ! {
     //This is the entry point, since the
     //linker looks for a function named _start
     //by default
+    let vga_buffer = 0xb8000 as *mut u8;
+    
+    for(i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; //Color byte cyan
+        }
+    }
+    
     loop{}
 }
 
